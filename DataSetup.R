86e5c655f786b153993ee52c3751796660b534e2
Isaiah Chastain <ipchastain42@tntech.edu>
2025-11-04 18:52:45 -0600

    Added DataSetup.R and Column Name Explanation

---
+++ DataSetup.R
@@ -0,0 +1,3 @@
+adults <- read.csv("./dataset/adult.data")
+colnames(adults) <- c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class")
+
