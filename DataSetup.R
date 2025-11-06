f906b8d212120b1e50db0874188297cb10dd145e
Isaiah Chastain <ipchastain42@tntech.edu>
2025-11-06 13:37:07 -0600

    Converted class from character to bool

--- DataSetup.R
+++ DataSetup.R
@@ -1,3 +1,10 @@
+install.packages("tidymodels")
+install.packages("ggplot2")
+library(tidymodels)
+library(ggplot2)
+
 adults <- read.csv("./dataset/adult.data")
 colnames(adults) <- c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class")
 
+adults[adults$class == " <=50K", "class"] <- 0    # sets the class <=50K to 0
+adults[adults$class == " >50K", "class"] <- 1     # sets the class >50K to 1
