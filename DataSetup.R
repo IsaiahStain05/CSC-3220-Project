a629fd408c0b550c1440cae571b27fbce46b6538
Isaiah Chastain <ipchastain42@tntech.edu>
2025-11-10 10:23:14 -0600

    Fixed logical/char discrepancy in class column

--- DataSetup.R
+++ DataSetup.R
@@ -8,10 +8,33 @@ library(dplyr)
 adults <- read.csv("./dataset/adult.data")
 colnames(adults) <- c("age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "class")
 
-adults[adults$class == " <=50K", "class"] <- 0    # sets the class <=50K to 0
-adults[adults$class == " >50K", "class"] <- 1     # sets the class >50K to 1
+adults[adults$class == " <=50K", "class"] <- FALSE    # sets the class <=50K to FALSE
+adults[adults$class == " >50K", "class"] <- TRUE     # sets the class >50K to TRUE
+adults$class <- as.logical(adults$class)
 
 
 cols <- c("workclass", "education", "marital-status", "occupation", "relationship", "sex", "native-country", "class")
 adults[cols] <- lapply(adults[cols], function(x) ifelse(x == " ?", NA, x))
 adults <- drop_na(adults)
+
+
+#Education has the highest correlation. Age and hours per week have moderate correlation. Capital-loss has low correlation.
+nums <- adults[, sapply(adults, is.numeric)]
+nums$class <- as.numeric(adults$class)
+corr_matrix <- cor(nums, use = "complete.obs")
+corr_with_income <- corr_matrix[, "class"]
+
+print(corr_with_income)
+
+#Display non-numeric correlations between income (>50k) and other census data
+non_numeric <- adults[, !sapply(adults, is.numeric)]
+dummy_vars <- model.matrix(~ . - 1, data = non_numeric)  # one-hot encode without intercept
+
+encoded_data <- cbind(dummy_vars, class = as.numeric(adults$class))
+
+cat_correlations <- cor(encoded_data, use = "complete.obs")[, "class"]
+
+cat_correlations_sorted <- sort(cat_correlations, decreasing = TRUE)
+
+cat("Correlations with income:\n")
+print(head(cat_correlations_sorted, 100))
